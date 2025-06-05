a7a3ef409160b2f46cac4765eb93000e24d0ee0d
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-05 23:08:18 +0200

    Refactor lamp toggle logic and update image display styles in index.html and script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Seleziono gli elementi che mi servono tramite il metodo .getElementById e li salvo in una costante
 
 const lampadinaOff = document.getElementById("lampOff");
-const lampadinaOn = document.getElementById("lampOn")
+const lampadinaOn = document.getElementById("lampOn");
 const button = document.getElementById("button");
 console.log(lampadinaOff.src);
 console.log(button.innerHTML);
@@ -9,16 +9,26 @@ console.log(button.innerHTML);
 // Quando avviene un certo evento (in questo caso il click sul bottone) devo far eseguire una funzione 
 
 button.addEventListener("click", function () {
-    if (button.innerHTML === "Accendi la lampadina") {
-        button.innerHTML = "Spegni la lampadina";
+
+    if (button.innerText === "Accendi la lampadina"
+        && lampadinaOff.style.display === "block"
+        && lampadinaOn.style.display === "none"
+    ) {
+        button.innerText = "Spegni la lampadina";
+        lampadinaOff.style.display = "none";
+        lampadinaOn.style.display = "block";
     } else {
-        button.innerHTML = "Accendi la lampadina";
-    }
-    if (lampadinaOn.className === "d-none") {
-        lampadinaOn.className = "d-block";
-        lampadinaOff.className = "d-none"
-    } else {
-        lampadinaOn.className = "d-none";
-        lampadinaOff.className = "d-block";
+        button.innerText = "Accendi la lampadina";
+        lampadinaOn.style.display = "none";
+        lampadinaOff.style.display = "block";
     }
 })
+
+
+// if (lampadinaOn.className === "d-none") {
+//     lampadinaOn.className = "d-block";
+//     lampadinaOff.className = "d-none"
+// } else {
+//     lampadinaOn.className = "d-none";
+//     lampadinaOff.className = "d-block";
+// }
