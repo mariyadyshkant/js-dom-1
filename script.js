c53a8b5b9d12cc433fc38c6ef39da227d752ca40
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-05 14:30:16 +0200

    Add lamp images and button functionality to index.html and script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Seleziono gli elementi che mi servono tramite il metodo .getElementById e li salvo in una costante
+
+const lampadinaOff = document.getElementById("lampOff");
+const lampadinaOn = document.getElementById("lampOn")
+const button = document.getElementById("button");
+console.log(lampadinaOff.src);
+console.log(button.innerHTML);
+
+// Quando avviene un certo evento (in questo caso il click sul bottone) devo far eseguire una funzione 
+
+button.addEventListener("click", function () {
+    button.innerHTML = "Spegni la lampadina";
+    lampadinaOn.
+})
